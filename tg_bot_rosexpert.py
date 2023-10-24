ae8bf4dcb868e972438892ef815bfdd2b13642e4
Polina <polinakiseleva4@gmail.com>
2023-10-24 23:10:17 +0200

    Расширение списка команд. Добавление раздела с инфо о компании. Добавление контактной инфо

--- tg_bot_rosexpert.py
+++ tg_bot_rosexpert.py
@@ -35,40 +35,67 @@ retriever = vectorstore.as_retriever()
 qa = ConversationalRetrievalChain.from_llm(llm=chat, retriever=retriever, memory=memory)
 
 
-# answer = qa(input('Введите ваш вопрос '))
-# print(answer['answer'])
-
 @bot.message_handler(commands=['start'])  # указываем, какие команды отслеживаем
 def start(message):
     bot.send_message(message.chat.id, "Привет! Чем могу помочь?")
 
 
+@bot.message_handler(commands=['aboutcompany'])
+def description(message):
+    bot.send_message(message.chat.id, "Центр сертификации Росэксперт выполняет сертификацию продуктов и услуг с 2012 "
+                                      "года. За это время оформили 28 000 документов для компаний России, Беларуси, "
+                                      "Казахстана. Подтверждаем качество и безопасность различной продукции: булочек, "
+                                      "игрушек, гидроциклонов.")
+
+
+@bot.message_handler(commands=['contacts'])
+def contacts(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = telebot.types.KeyboardButton('WhatsApp')
+    item2 = telebot.types.KeyboardButton('Телефон')
+    item3 = telebot.types.KeyboardButton('Почта')
+    item4 = telebot.types.KeyboardButton('Адрес')
+
+    markup.add(item1, item2, item3, item4)
+    bot.send_message(message.chat.id, 'Выберите удобный способ связи из предложенных вариантов', reply_markup=markup)
+
+
 @bot.message_handler(content_types=['text'])  # отслеживаем только ввод текста
 def get_text_messages(message):
+    if message.text == 'WhatsApp':
+        answer = 'Ссылка на наш WhatsApp \nhttps://wtsp.cc/79673550951'
+        bot.send_message(message.chat.id, answer)  # ответ бота
 
-    answer = qa(message.text)
-    # """
-    #
-    # :param message:
-    # :return:
-    # """
-    # reply = ''
-    # response = openai.ChatCompletion.create(
-    #     messages=[
-    #         {"role": "user", "content": message.text}
-    #     ],  # какой текст будет принимать бот в качестве запроса
-    #     model="gpt-4",  # модель чата
-    #     max_tokens=300,  # максимальное количество возвращаемых слов
-    #     temperature=1,  # креативность
-    #     n=1,  # количество ответов
-    #     stop=None  # стоп-слово, которое стопарит чат
-    # )
-    # if response and response.choices:  # если есть ответ и его варианты
-    #     reply = response.choices[0].message.content
-    # else:
-    #     reply = 'Что-то пошло не по плану!'
+    elif message.text == 'Телефон':
+        answer = 'Наш номер телефона \n8 800 775-27-45'
+        bot.send_message(message.chat.id, answer)  # ответ бота
 
-    bot.send_message(message.chat.id, answer['answer'])  # ответ бота
+    elif message.text == 'Почта':
+        answer = 'Наша почта \ninfo@rosexperts.ru'
+        bot.send_message(message.chat.id, answer)  # ответ бота
+
+    elif message.text == 'Адрес':
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = telebot.types.KeyboardButton('Москва')
+        item2 = telebot.types.KeyboardButton('Калининград')
+
+        markup.add(item1, item2)
+        bot.send_message(message.chat.id, 'Выберите город', reply_markup=markup)
+
+    elif message.text == 'Москва':
+        answer = 'Наш адрес: \nг. Москва, ул. Расплетина, д. 12, корп. 1 (почтовый индекс 123060)\n' \
+                 'График работы: понедельник-пятница 09:00-18:00'
+        bot.send_message(message.chat.id, answer)
+
+    elif message.text == 'Калининград':
+        answer = 'Наш адрес: \nг. Калининград, ул. Зоологическая, д. 50 (почтовый индекс 236022)\n' \
+                 'График работы: понедельник-пятница 09:00-18:00'
+        bot.send_message(message.chat.id, answer)
+
+    else:
+        answer = qa(message.text)
+        answer = answer['answer']
+        bot.send_message(message.chat.id, answer)  # ответ бота
 
 
 # чтобы бот не останавливался
